a30330616195b5d601bb7f71b6e2b28ffca0e262
himalik <him4lik@gmail.com>
2025-09-27 13:38:40 +0530

    changes

--- web_ecom/settings.py
+++ web_ecom/settings.py
@@ -145,10 +145,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-if DEBUG:
-    MEDIA_URL = "/media/"
-    MEDIA_ROOT = "media"
-    DEFAULT_FILE_STORAGE = "django.core.files.storage.FileSystemStorage"
+MEDIA_URL = "/web/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+DEFAULT_FILE_STORAGE = "django.core.files.storage.FileSystemStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
